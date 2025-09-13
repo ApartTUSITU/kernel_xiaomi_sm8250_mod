5b7350ba38e4f1bd5fe836b876976a93fe2b0016
ApartTUSITU <aptusitu@outlook.com>
2025-09-13 21:20:00 +0800

    chore: Remove support for device codename "thyme" and "apollo", Update README.md

--- buildall.sh
+++ buildall.sh
@@ -2,13 +2,11 @@
 set -e
 
 bash build.sh psyche
-bash build.sh thyme
 bash build.sh umi
 bash build.sh munch
 bash build.sh lmi
 bash build.sh cmi
 bash build.sh cas
-bash build.sh apollo
 bash build.sh alioth
 bash build.sh elish
 bash build.sh enuma
@@ -16,13 +14,11 @@ bash build.sh dagu
 bash build.sh pipa
 
 bash build.sh psyche ksu
-bash build.sh thyme ksu
 bash build.sh umi ksu
 bash build.sh munch ksu
 bash build.sh lmi ksu
 bash build.sh cmi ksu
 bash build.sh cas ksu
-bash build.sh apollo ksu
 bash build.sh alioth ksu
 bash build.sh elish ksu
 bash build.sh enuma ksu
